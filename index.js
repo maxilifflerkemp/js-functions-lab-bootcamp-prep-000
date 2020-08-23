057c37aa6aceb312261298395442fef2e1b4930b
Maximiliane Liffler <maxi-liffler@web.de>
2020-08-23 19:44:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function happyHolidaysTo(name) { return `Happy holidays, ${name}!` }
 
 var holiday = 'Independence Day', name = 'you'
 
-function happyHolidayTo(holiday, name) { return
+function happyCustomHolidayTo(holiday, name) { return
 `Happy ${holiday}, ${name}!` }
 
 var days = "20"
